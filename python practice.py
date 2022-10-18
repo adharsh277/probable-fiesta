d9f4185771e468358916551c88dccbb80f528f7f
adharsh277 <114822057+adharsh277@users.noreply.github.com>
2022-10-18 22:49:40 +0530

    Add files via upload

---
+++ python practice.py
@@ -0,0 +1,54 @@
+from calendar import c
+from re import A
+from tkinter import Y
+
+
+x = 30
+y = 67
+z = x+y
+print(z)
+A = "you are the real hero"
+B = "of the curent sceniro they may think you can't make the world free but i know yo can son do it"
+c = "your cordinates for the earth will be the "
+d = "768'536208"
+print(A+B+c+d)
+myname = 'john samuel umitti is not goona good at you'
+print(myname+"but richard is  gonna create so ething you  can't feel")
+x, y = 90, 765
+print(x*22)
+print(x*9)
+print(x/55)
+# now it s for modifictaion
+x = "we are creating new world with only peoples wish that should be meta version"
+print(x[-1:])
+print(x[1:])
+print(x[-9:-9])
+print(x[-9:8])
+print(x[9:-1])
+print(x.upper())
+print(x.lower())
+print(x.split())
+y = "You just have to decide what kind of man you want to grow up to be humans Whoever that man is he's going to change the world "
+print(x.upper())
+print(x.lower())
+print(x.split())
+print(x.replace('e', 'y'))
+print(x[1:])
+# now its time for the booleans
+print(bool(False))
+print(bool(True))
+# arithemetic operator
+print(10+381+67+80+817)
+print(10+38+67+874)
+print(10+67+80+87)
+print(138+675+80+585)
+# just my creative
+print(int(input()))
+print(x)
+x = "good night"
+y = 'day dreamers'
+print(x+y)
+# hyhj
+x = "hjki"
+y = ""
+print(x+y)
